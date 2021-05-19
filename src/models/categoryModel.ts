09b9cd8ad21e4816dd8f0607d8b304894099e738
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-05-19 12:43:27 +0530

    added extra params to category model

--- src/models/categoryModel.ts
+++ src/models/categoryModel.ts
@@ -6,6 +6,25 @@ const categorySchema = new schema({
     type: String,
     required: [true, "Name of category is required"],
   },
+  extraParameters: [
+    {
+      name: {
+        type: String,
+        required: [true, "Name of the paramater is required"],
+      },
+      icon: {
+        type: String,
+        required: [true, "Icon of the paramater is required"],
+      },
+      type: {
+        type: String,
+        required: [
+          true,
+          "Type of the paramater is required (e.g Text, Tel, Email, Text Area)",
+        ],
+      },
+    },
+  ],
 });
 categorySchema.index({ name: "text" });
 
