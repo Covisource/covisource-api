84025acfc1d1dcb5189d757ce4fa13c7924f7d24
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-05-19 13:55:24 +0530

    changed newCategory route to accept new params

--- src/models/categoryModel.ts
+++ src/models/categoryModel.ts
@@ -10,18 +10,22 @@ const categorySchema = new schema({
     {
       name: {
         type: String,
-        required: [true, "Name of the paramater is required"],
+        required: [true, "Name of the parameter is required"],
       },
       icon: {
         type: String,
-        required: [true, "Icon of the paramater is required"],
+        required: [true, "Icon of the parameter is required"],
       },
       type: {
         type: String,
         required: [
           true,
-          "Type of the paramater is required (e.g Text, Tel, Email, Text Area)",
+          "Type of the parameter is required (e.g Text, Tel, Email, Text Area)",
         ],
+      },
+      isRequired: {
+        type: Boolean,
+        required: false,
       },
     },
   ],
